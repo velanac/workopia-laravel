aa8fa353607231f44b3773d1b0ce344b4c244236
velanac <vidmedia@gmail.com>
2024-09-29 11:28:17 +0200

    Update form

--- app/Http/Controllers/JobController.php
+++ app/Http/Controllers/JobController.php
@@ -6,6 +6,7 @@ use App\Models\Job;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class JobController extends Controller
 {
@@ -89,9 +90,43 @@ class JobController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id): string
+    public function update(Request $request, Job $job): string
     {
-        return 'Update';
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'salary' => 'required|integer',
+            'tags' => 'nullable|string',
+            'job_type' => 'required|string',
+            'remote' => 'required|boolean',
+            'requirements' => 'nullable|string',
+            'benefits' => 'nullable|string',
+            'address' => 'nullable|string',
+            'city' => 'required|string',
+            'state' => 'required|string',
+            'zipcode' => 'nullable|string',
+            'contact_email' => 'required|string',
+            'contact_phone' => 'nullable|string',
+            'company_name' => 'required|string',
+            'company_description' => 'nullable|string',
+            'company_logo' => 'nullable|image|mimes:jpeg,jpg,png,gif|max:2048',
+            'company_website' => 'nullable|url'
+        ]);
+
+        // Check for imate
+        if ($request->hasFile('company_logo')) {
+            // Delete old logo
+            Storage::delete('public/logos/' . basename($job->company_logo));
+            // Store the file and get path
+            $path = $request->file('company_logo')->store('logos', 'public');
+
+            $validatedData['company_logo'] = $path;
+        }
+
+        // Submit to database
+        $job->update($validatedData);
+
+        return redirect()->route('jobs.index')->with('success', 'Job listing created successfully!');
     }
 
     /**
